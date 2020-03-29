28bbafd419fcf3e0a8e2b20e75e077d23a124ebd
dulvin <dulvinw@gmail.com>
2020-03-29 15:59:34 +0530

    Add dependencies for Rest Assured

---
+++ src/main/java/com/dulvinw/springboot/webapprestassuredtest/webapprestassuredtest/WebappRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.dulvinw.springboot.webapprestassuredtest.webapprestassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappRestAssuredTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappRestAssuredTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dulvinw/springboot/webapprestassuredtest/webapprestassuredtest/WebappRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulvinw.springboot.webapprestassuredtest.webapprestassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappRestAssuredTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
